0f6ac260b5a509beac2641cb195d0b57aa2b33e5
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-02-23 23:41:14 +0700

    Create connection to database mysql

---
+++ database.go
@@ -0,0 +1,21 @@
+package godatabase
+
+import (
+	"database/sql"
+	"time"
+)
+
+func GetConnection() *sql.DB {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/go_database")
+	if err != nil {
+		panic(err)
+	}
+
+	// using DB
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+	return db
+
+}
