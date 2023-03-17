c246342a364c00da4987d14a30f29a57f9e02fa3
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-03-17 15:17:18 +0700

    Query SQL

--- sql_test.go
+++ sql_test.go
@@ -19,3 +19,27 @@ func TestExecSql(t *testing.T) {
 
 	fmt.Println("Success insert new customer")
 }
+
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	script := "SELECT idcustomer, name FROM customer"
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var idcustomer int
+		var name string
+		rows.Scan(&idcustomer, &name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Id: ", idcustomer)
+		fmt.Println("Name: ", name)
+	}
+}
