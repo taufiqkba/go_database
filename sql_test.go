6124c4e4e7a052754d9416e1898afae8e55c0b45
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-03-24 16:26:52 +0700

    Query MySQL with several data type in Go

--- sql_test.go
+++ sql_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestExecSql(t *testing.T) {
@@ -42,4 +43,36 @@ func TestQuerySql(t *testing.T) {
 		fmt.Println("Id: ", idcustomer)
 		fmt.Println("Name: ", name)
 	}
+}
+
+func TestQuerySQLComplex(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT idcustomer, name, email, balance, rating, created_at, birth_date, married FROM customer"
+	rows, err := db.QueryContext(ctx, script)
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	for rows.Next() {
+		var idcustomer int32
+		var name, email string
+		var balance int32
+		var rating float64
+		var createdAt, birthDate time.Time
+		var married bool
+
+		err := rows.Scan(&idcustomer, &name, &email, &balance, &rating, &createdAt, &birthDate, &married)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("\nId: ", idcustomer, "\nName: ", name, "\nEmail: ", email, "\nBalance: ", balance, "\nRating: ", rating, "\nDate Created: ", createdAt, "\nBirth Date: ", birthDate, "\nMarried Status: ", married)
+	}
+
 }
