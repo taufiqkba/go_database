f3601d03bcaa644b86b7438086b29df93e1f9ceb
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-03-25 15:04:48 +0700

    Case SQL Injection

--- sql_test.go
+++ sql_test.go
@@ -92,3 +92,32 @@ func TestQuerySQLComplex(t *testing.T) {
 	}
 
 }
+
+func TestSqlInjection(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "admin'; #"
+	password := "salah"
+
+	script := "SELECT username FROM user WHERE username = '" + username + "' AND password = '" + password + "' LIMIT 1"
+	rows, err := db.QueryContext(ctx, script)
+	fmt.Println(script)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		var username string
+		err := rows.Scan(&username)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Success login,", username)
+	} else {
+		fmt.Println("Failed Login!")
+	}
+}
